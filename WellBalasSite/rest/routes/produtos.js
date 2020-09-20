7a0124a0ac49f759bed96e42220b8c060e7b1310
Your Name <nata.silva@bandtec.com.br>
2020-09-19 22:33:02 -0300

    api sendo feita

---
+++ WellBalasSite/rest/routes/produtos.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+
+// RETORNA TODOS OS PRODUTOS
+router.get('/', (req, res, next) => {
+    res.status(200).send({
+        mensagem: 'Usando Get dentro da rota produto'
+    })
+});
+
+
+//INSERE UM PRODUTO
+router.post('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Usando GET dentro da rota produto'
+    })
+});
+
+
+// RETORNA OS DADOS DE UM PRODUTO
+router.get('/:id_produto', (req, res, next) => {
+    const id = req.params.id_produto;
+
+    if(id === 'especial'){
+        res.status(200).send({
+            mensagem: 'Voce descobriu um id especial',
+            id: id
+        });
+    } else {
+        res.status(200).send({
+            mensagem: 'Voce passou um id'
+        })
+    }  
+});
+
+router.patch('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Usando PATCH dentro da rota produto'
+    })
+});
+
+router.delete('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Usando DELETE dentro da rota produto'
+    })
+});
+
+
+
+module.exports = router;
